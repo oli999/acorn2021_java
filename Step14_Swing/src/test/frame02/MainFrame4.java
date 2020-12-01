d0ad66f7ccc7adf20b8481cad49f5167355dbb47
kimgura <oli999@naver.com>
2020-12-01 12:19:16 +0900

    Step14_Swing 예제 추가함

--- Step14_Swing/src/test/frame02/MainFrame4.java
+++ Step14_Swing/src/test/frame02/MainFrame4.java
@@ -8,32 +8,12 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
-public class MainFrame3 extends JFrame{
-	//필드에 ActionListener 
-	ActionListener listener=new ActionListener() {
-		//인자로 전달되는 ActionEvent 객체에는 눌러진 버튼의 정보가 들어 있다.
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			//이벤트가 일어난 UI (Button) 의 ActionCommand  값을 읽어온다.
-			String command=e.getActionCommand();
-			// 문자열(String) 의 내용을 비교 할때는 == 를 사용하지말고
-			// .equals() 메소드를 이용해야 한다. 
-			if(command.equals("send")) {
-				JOptionPane.showMessageDialog(MainFrame3.this, 
-						"전송합니다.");
-			}else if(command.equals("update")) {
-				JOptionPane.showMessageDialog(MainFrame3.this, 
-						"수정합니다.");
-			}else if(command.equals("delete")) {
-				JOptionPane.showMessageDialog(MainFrame3.this, 
-						"삭제합니다.");
-			}
-		}
-	};
+public class MainFrame4 extends JFrame implements ActionListener{
 	
 	//생성자
-	public MainFrame3(String title) {
+	public MainFrame4(String title) {
 		super(title); //부모생성자에 전달하기 
+	
 		//레이아웃 메니저 지정하기
 		setLayout(new FlowLayout()); //물 흐르듯이 배치되는 레이아웃 
 		//버튼 3 개를 만들고 
@@ -45,9 +25,9 @@ public class MainFrame3 extends JFrame{
 		add(updateBtn);
 		add(deleteBtn);
 		//버튼에 리스너 등록하기
-		sendBtn.addActionListener(listener);
-		updateBtn.addActionListener(listener);
-		deleteBtn.addActionListener(listener);
+		sendBtn.addActionListener(this);
+		updateBtn.addActionListener(this);
+		deleteBtn.addActionListener(this);
 		//버튼에 ActionCommand 를 원하는대로 지정 할수 있다. 
 		sendBtn.setActionCommand("send");
 		updateBtn.setActionCommand("update");
@@ -56,13 +36,29 @@ public class MainFrame3 extends JFrame{
 	
 	
 	public static void main(String[] args) {
-		MainFrame3 f=new MainFrame3("메인 프레임");
+		MainFrame4 f=new MainFrame4("메인 프레임");
 		//위치와 크기 지정
 		f.setBounds(100, 100, 500, 300);
 		//프레임을 닫으면 프로세스가 종료 되도록(앱이 종료 되도록)
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		//화면상에 실제 보이게 한다. 
 		f.setVisible(true);
+	}
+
+	// MainFrame4 가 implements ActionListener 했으므로 강제 구현된 메소드 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		//이벤트가 일어난 UI (Button) 의 ActionCommand  값을 읽어온다.
+		String command=e.getActionCommand();
+		// 문자열(String) 의 내용을 비교 할때는 == 를 사용하지말고
+		// .equals() 메소드를 이용해야 한다. 
+		if(command.equals("send")) {
+			JOptionPane.showMessageDialog(this, "전송합니다.");
+		}else if(command.equals("update")) {
+			JOptionPane.showMessageDialog(this, "수정합니다.");
+		}else if(command.equals("delete")) {
+			JOptionPane.showMessageDialog(this, "삭제합니다.");
+		}
 	}
 }
 
