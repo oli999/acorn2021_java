dfef5284d4d31f247706e4a67f07d36dd17455b6
kimgura <oli999@naver.com>
2020-12-03 17:44:14 +0900

    Step17 에 예제 추가함

--- Step17_InputOutput/src/test/main/MainClass14.java
+++ Step17_InputOutput/src/test/main/MainClass14.java
@@ -1,11 +1,12 @@
 package test.main;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class MainClass13 {
+public class MainClass14 {
 	public static void main(String[] args) {
 		/* 
 		 *  c:/myFolder/memo.txt 파일에 저장된 문자열을 읽어와서
@@ -17,21 +18,16 @@ public class MainClass13 {
 		File f=new File("c:/myFolder/memo.txt");
 		try {
 			FileReader fr=new FileReader(f);
-			//아래 세줄의 코드를 무한 루프 돌면서 수행하다가 
-			//read() 메소드가 -1 을 리턴하면 루프를 탈출하기 
+			BufferedReader br=new BufferedReader(fr);
+			//아래의 두줄의 코드를 무한 루프에서 수행을 하다가
+			//readLine() 메소드가 null 을 리턴하면 반복문 탈출
 			while(true) {
-				//read() 메소드는 더이상 읽을 데이터가 없으면 -1 을 리턴한다. 
-				int code=fr.read();
-				//만일 code 가 -1 과 같으면 반복문 while 을 탈출 하세요
-				if(code==-1) {
-					//break  예약어는 반복문을 탈출하는 예약어이다. 
-					break; 
+				String line=br.readLine();
+				if(line==null) {
+					break;
 				}
-				//code 에 해당하는 문자 값을 얻어내서 
-				char ch=(char)code;
-				//개행기호 없이 출력하기 
-				System.out.print(ch);
-			}
+				System.out.println(line);	
+			}			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
