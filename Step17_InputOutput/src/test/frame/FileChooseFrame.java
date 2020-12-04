6d6f8a7aa71c9ce37e352f26d2efc8b6b4516d60
kimgura <oli999@naver.com>
2020-12-04 17:27:42 +0900

    Step17 GUI 예제 추가함

--- Step17_InputOutput/src/test/frame/FileChooseFrame.java
+++ Step17_InputOutput/src/test/frame/FileChooseFrame.java
@@ -2,38 +2,110 @@ package test.frame;
 
 import java.awt.BorderLayout;
 import java.awt.Button;
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class FileChooseFrame extends JFrame implements ActionListener{
+	//필요한 필드정의하기 (모든 메소드에서 접근 가능한 자원)
+	JButton selectBtn, selectBtn2;
+	JTextArea area;
+	
 	//생성자
 	public FileChooseFrame(String title) {
 		super(title);
 		setLayout(new BorderLayout());
-		JButton selectBtn=new JButton("파일 선택하기");
+		//버튼의 참조값을 필드에 저장해 놓는다 (다른 메소드에서 참조해서 사용하려고)
+		selectBtn=new JButton("파일 선택하기");
 		add(selectBtn, BorderLayout.NORTH);
 		selectBtn.addActionListener(this);
+		
+		selectBtn2=new JButton(".txt 파일 선택하기");
+		add(selectBtn2, BorderLayout.SOUTH);
+		selectBtn2.addActionListener(this);
+		
+		//JTextArea 객체 생성
+		area=new JTextArea();
+		add(area, BorderLayout.CENTER);
+		area.setBackground(Color.YELLOW);
+		
 	}
 	
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
+	public void actionPerformed(ActionEvent e) {
+		//액션이 일어난 UI(JButton) 의 참조값 얻어오기
+		Object obj=e.getSource();
+		if(obj == selectBtn) { //파일 선택 버튼을 눌렀을때
+			this.selectGeneral();
+		}else if(obj == selectBtn2) {// .txt 파일 선택 버튼을 눌렀을때 
+			selectText();
+		}
+		System.out.println("actionPerformed() 메소드가 종료 됩니다.");
+	}
+	//일반 파일을 선택하는 메소드
+	public void selectGeneral() {
+		// c:/myFolder 에서 시작되는 JFileChooser  객체 생성 
 		JFileChooser fc=new JFileChooser("c:/myFolder");
+		//파일을 선택해서 열수 있는 다이얼로그 띄우고 어떤 작업을 했는지 결과를 int 로 받기
 		int result=fc.showOpenDialog(this);
 		
 		if(result == JFileChooser.APPROVE_OPTION) {//파일을 선택하고 확인을 눌렀을때 
+			//선택한 파일을 Access 할수 있는 File 객체의 참조값 얻어오기
 			File selectedFile=fc.getSelectedFile();
-			selectedFile.delete();
+			//선택한 파일의 이름 얻어오기 
+			String fileName=selectedFile.getName();
+			//메세지 다이얼로그 띄우기 
+			JOptionPane.showMessageDialog(this, fileName+" 파일을 선택했네요?");
 		}else if(result == JFileChooser.CANCEL_OPTION) { //취소 버튼을 눌렀을때 
-			
-		}
-		
-		System.out.println("actionPerformed() 메소드가 종료 됩니다.");
+			JOptionPane.showMessageDialog(this, "취소 버튼을 눌렀네요?");
+		}else if(result == JFileChooser.ERROR_OPTION) {
+			JOptionPane.showMessageDialog(this, "에러");
+		}		
 	}
+	//텍스트 파일을 선택하는 메소드
+	public void selectText() {
+		JFileChooser fc=new JFileChooser("c:/myFolder");
+		//파일 확장자가 .txt 인 파일만 선택할수 있도록 필터 설정 
+		FileNameExtensionFilter filter=
+				new FileNameExtensionFilter("텍스트 파일", "txt");
+		fc.setFileFilter(filter);
+		//선택된 텍스트 문서에 저장된 문자열을 읽어서 콘솔창에 모두 출력 할수 있숩니까?
+		int result=fc.showOpenDialog(this);
+		if(result == JFileChooser.APPROVE_OPTION) {
+			File selectedFile=fc.getSelectedFile();
+			try {
+				//선택된 파일을 Access 할수 있는 File 객체를 이용해서 
+				//문자열을 읽어들일수 있는 FileReader 객체 생성하기 
+				FileReader fr=new FileReader(selectedFile);
+				BufferedReader br=new BufferedReader(fr);
+				while(true) {//반복문 돌면서
+					//문자열을 한줄씩 읽어들인다.
+					String line=br.readLine();
+					//만일 더이상 읽어들일게 없다면
+					if(line==null) {
+						break; //반복문 탈출
+					}
+					//읽어들인 문자열 콘솔창에 출력
+					//System.out.println(line);
+					//읽어들인 문자열을 JTextArea 에 출력해 보시렵니까?
+					area.append(line+"\r\n");
+				}
+			}catch(Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+
 	//메인 메소드 
 	public static void main(String[] args) {
 		FileChooseFrame f=new FileChooseFrame("파일 선택하는 프레임");
---
+++ Step17_InputOutput/src/test/frame/TextEditor.java
@@ -0,0 +1,100 @@
+package test.frame;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JTextArea;
+
+public class TextEditor extends JFrame implements ActionListener{
+	//필요한 static final 상수 정의하기 
+	public static final String COMMAND_NEW="new";
+	public static final String COMMAND_OPEN="open";
+	public static final String COMMAND_SAVE="save";
+	JTextArea area;
+	
+	//생성자
+	public TextEditor(String title) {
+		super(title);
+		setLayout(new BorderLayout());
+		
+		//메뉴1
+		JMenu menu1=new JMenu("File");
+		//메뉴2
+		JMenu menu2=new JMenu("Help");
+		
+		//메뉴바
+		JMenuBar mb=new JMenuBar();
+		//메뉴바에 메뉴 추가
+		mb.add(menu1);
+		mb.add(menu2);
+		//프레임에 메뉴바 추가
+		this.setJMenuBar(mb);
+		
+		//메뉴 아이템1
+		JMenuItem item1=new JMenuItem("New");
+		//메뉴 아이템2
+		JMenuItem item2=new JMenuItem("Open");
+		//메뉴 아이템3
+		JMenuItem item3=new JMenuItem("Save");
+		
+		//메뉴 아이템을 메뉴에 추가하기 
+		menu1.add(item1);
+		menu1.add(item2);
+		menu1.add(item3);
+		
+		//아이템에 액션 리스너 등록
+		item1.addActionListener(this);
+		item2.addActionListener(this);
+		item3.addActionListener(this);
+		
+		//아이템에 액션 command 지정
+		item1.setActionCommand(TextEditor.COMMAND_NEW);
+		item2.setActionCommand(TextEditor.COMMAND_OPEN);
+		//동일한 클래스에 있는 static 필드를 참조할때는 클래스명 생략 가능
+		item3.setActionCommand(COMMAND_SAVE);
+		
+		//JTextArea
+		area=new JTextArea();
+		add(area, BorderLayout.CENTER);
+		area.setBackground(Color.PINK);
+		//일단 안보이게 해놓는다
+		area.setVisible(false);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		//액션 command 를 읽어온다. 
+		String command=e.getActionCommand();
+		if(command.equals(COMMAND_NEW)) {
+			area.setVisible(true);
+			area.grabFocus();//포커스 주기
+		}else if(command.equals(COMMAND_OPEN)) {
+			JFileChooser fc=new JFileChooser("c:/myFolder");
+			int result=fc.showOpenDialog(this);
+			
+		}else if(command.equals(COMMAND_SAVE)) {
+			JFileChooser fc=new JFileChooser("c:/myFolder");
+			int result=fc.showSaveDialog(this);
+			
+		}
+	}
+	public static void main(String[] args) {
+		TextEditor te=new TextEditor("텍스트 편집기");
+		te.setBounds(100, 100, 500, 500);
+		te.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		te.setVisible(true);
+	}
+	
+}
+
+
+
+
+
