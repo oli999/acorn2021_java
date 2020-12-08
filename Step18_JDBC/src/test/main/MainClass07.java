868cd8ffc7c0d285aee81122f21bbb68dfefdb19
kimgura <oli999@naver.com>
2020-12-08 17:15:00 +0900

    Step18_JDBC 에 예제 추가함

--- Step18_JDBC/src/test/main/MainClass07.java
+++ Step18_JDBC/src/test/main/MainClass07.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.util.Scanner;
 
 import test.member.dto.MemberDto;
+import test.util.DBConnect;
 
 /*
  *  Scanner 객체를 이용해서 검색할 회원의 번호를 입력 받아서
@@ -25,7 +26,39 @@ public class MainClass07 {
 		Connection conn=null;
 		PreparedStatement pstmt=null;
 		ResultSet rs=null;
-		
+		try {
+			conn=new DBConnect().getConn();
+			String sql="SELECT name, addr FROM member WHERE num=?";
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setInt(1, num);
+			rs=pstmt.executeQuery();
+			if(rs.next()) {//select 된 결과가 있다면
+				//MemberDto 객체를 생성해서 
+				dto=new MemberDto();
+				//setter 메소드를 이용해서 값을 담는다. 
+				dto.setNum(num);
+				//ResultSet 객체에 있는 값도 얻어와서 담아 준다.
+				String name=rs.getString("name");
+				dto.setName(name);
+				dto.setAddr(rs.getString("addr"));
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(rs!=null)rs.close();
+				if(pstmt!=null)pstmt.close();
+				if(conn!=null)conn.close();
+			}catch(Exception e) {}
+		}
+		//위의 try~catch~finally 절이 수행된후에 SELECT 된 결과가 있는지 없는지를
+		//여기에서 쉽게 판별할수 있는 방법이 있나요?
+		if(dto == null) {
+			System.out.println(num + " 번 회원은 존재 하지 않습니다.");
+		}else {
+			System.out.println("번호:"+dto.getNum()+
+					", 이름:"+dto.getName()+", 주소:"+dto.getAddr());
+		}
 	}
 }	
 
