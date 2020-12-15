0e74659bc26c5c60ad8002df68ab5c2338768317
kimgura <oli999@naver.com>
2020-12-15 11:25:25 +0900

    Step19 example6 추가함

---
+++ Step19_Socket/src/example6/ClientFrame.java
@@ -0,0 +1,213 @@
+package example6;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import org.json.JSONObject;
+/*
+ *  - 대화방에 현재 누가 참여하고 있는지 목록 출력하기
+ *  - 누가 새로 입장했는지 메세지 출력하기
+ *  - 누가 퇴장 했는지 메세지 출력하기
+ *  
+ *  서버에 전달되는 메세지의 종류
+ *  
+ *  1. 일반 대화 메세지
+ *     {"type":"msg", "name":"김구라", "content":"안녕하세요!"}
+ *  2. 누군가 입장했다는 메세지
+ *     {"type":"enter", "name":"해골"}
+ *  3. 누군가 퇴장했다는 메세지
+ *     {"type":"out", "name":"원숭이"}
+ *  4. 참여자 목록 메세지
+ *     {"type":"members", "list":["주뎅이","덩어리","개다리"]}
+ * 
+ */
+public class ClientFrame extends JFrame 
+		implements ActionListener, KeyListener{
+	//필요한 필드 정의하기 
+	JTextField tf;
+	Socket socket;
+	BufferedWriter bw;
+	BufferedReader br;
+	JTextArea ta;
+	String chatName; //대화명을 저장할 필드 
+	
+	//생성자
+	public ClientFrame(String title) {
+		super(title);
+		setLayout(new BorderLayout());
+		
+		//페널을 만들어서 프레임의 위쪽에 배치하기 
+		JPanel topPanel=new JPanel();
+		topPanel.setBackground(Color.YELLOW);
+		add(topPanel, BorderLayout.NORTH);
+		
+		//프레임에 UI 배치하기
+		tf=new JTextField(10);
+		JButton sendBtn=new JButton("Send");
+		topPanel.add(tf);
+		topPanel.add(sendBtn);
+		//버튼 리스너 등록하기
+		sendBtn.addActionListener(this);
+		//체팅 메세지를 출력할 TextArea 객체를 생성해서 
+		ta=new JTextArea();
+		//스크롤 가능한 UI 에 포장후 
+		JScrollPane scPane=new JScrollPane(ta,
+				JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		//스크롤 가능한 UI 를 프레임의 가운데에 배치한다. 
+		add(scPane, BorderLayout.CENTER);
+		//오직 출력 용도로 사용하기 위해 
+		ta.setEditable(false);
+		//JTextField 에 KeyListener 등록하기
+		tf.addKeyListener(this);
+		
+		//소켓 접속하기 
+		connect();
+	}
+	
+	public static void main(String[] args) {
+		//프레임객체 생성하면서 프레임의 제목을 chatting 으로 지정하기 
+		ClientFrame f=new ClientFrame("chatting");
+		//프레임의 x좌표, y좌표, width, height 설정하기
+		f.setBounds(100, 100, 500, 500);
+		//프레임을 닫을때 프로세스도 종료 되도록 한다. 
+		f.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		//프레임을 화면상에 보이게 하기 
+		f.setVisible(true);
+	}
+	
+	//Socket 서버에 접속을 하는 메소드
+	public void connect() {
+		//대화명을 입력 받아서 필드에 저장하기
+		chatName=JOptionPane.showInputDialog(this, "대화명을 입력하세요.");
+		if(chatName == null || chatName.equals("")) {
+			chatName="바보";
+		}
+		try {
+			//소켓객체 
+			socket=new Socket("14.63.164.99", 5000);
+			//서버에 문자열을 출력할 객체 
+			bw=new BufferedWriter
+					(new OutputStreamWriter(socket.getOutputStream()));
+			//서버가 전송하는 문자열을 읽어들일 객체 
+			br=new BufferedReader
+					(new InputStreamReader(socket.getInputStream()));
+			//새로운 스레드를 시작 시켜서 서버에서 문자열이 도착하는지 지속적으로 대기한다.
+			new ClientThread().start();
+			//내가 입장한다고 서버에 메세지를 보낸다.
+			//JSONObject 에 정보를 담고 
+			JSONObject jsonObj=new JSONObject();
+			jsonObj.put("type", "enter");
+			jsonObj.put("name", chatName);
+			//JSONObject 에 담긴정보를 JSON 문자열로 만들어서 
+			String msg=jsonObj.toString();
+			//서버에 출력하기 
+			bw.write(msg);
+			bw.newLine();
+			bw.flush();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//전송 버튼을 눌렀을때 
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		sendMessage();
+	}
+	//서버에서 문자열이 전송되는지 지속적으로 대기하는 스레드 객체를 생성할 클래스 설계
+	class ClientThread extends Thread{
+		@Override
+		public void run() {
+			while(true) {
+				try {
+					//대기하다가 문자열이 도착하면 메소드가 리턴한다.
+					String line=br.readLine();
+					//도착된 메세지를 JTextArea 에 개행기호와 함께 추가하기
+					ta.append(line+"\r\n");
+					//출력할 문서의 높이
+					int height=ta.getDocument().getLength();
+					//높이 만큼 JTextArea 를 스크롤시켜서 가장 아래에 있는 문자열이 보이게
+					ta.setCaretPosition(height);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		//만일 엔터키를 눌렀다면 
+		if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+			sendMessage();
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	//메세지를 전송하는 메소드
+	public void sendMessage() {
+		//전송할 문자열
+		String msg=tf.getText();
+		try {
+			JSONObject jsonObj=new JSONObject();
+			jsonObj.put("type", "msg");
+			jsonObj.put("name", chatName);
+			jsonObj.put("content", msg);
+			String json=jsonObj.toString();
+			bw.write(json);
+			bw.newLine();
+			bw.flush();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		tf.setText("");
+	}
+}// class ClientFrame
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Step19_Socket/src/example6/ServerMain.java
@@ -0,0 +1,105 @@
+package example6;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServerMain {
+	//접속한 클라이언트를 응대하는 스레드 객체의 목록을 담을 필드
+	static List<ServerThread> threadList;
+	
+	public static void main(String[] args) {
+		//ArrayList 객체 생성해서 필드에 저장하기
+		threadList=new ArrayList<>();
+		
+		ServerSocket serverSocket=null;
+		try {
+			//5000 번 통신 port 를 열고 클라이언트의 접속을 기다린다.
+			serverSocket=new ServerSocket(5000);
+			/*
+			 *  .accept() 메소드는 클라이언트가 접속을 해야지 리턴하는 메소드이다.
+			 *  클라이언트가 접속을 해오면 해당 클라이언트와 연결된 Socket 객체의
+			 *  참조값을 리턴한다. 
+			 */
+			while(true) {
+				System.out.println("클라이언트의 Socket 접속을 기다립니다...");
+				//1. 클라이언트가 접속을 해오면 
+				Socket socket=serverSocket.accept();
+				//2. 새로운 스레드를 시작 시킨다. 
+				ServerThread t=new ServerThread(socket);
+				t.start();
+				//3. 시작된 스레드의 참조값을 목록에 누적 시킨다.
+				threadList.add(t);
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("ServerMain main 메소드가 종료 됩니다.");
+	}
+	//스레드 클래스 설계
+	static class ServerThread extends Thread{
+		//필드 (클라이언트와 연결된 Socket 객체의 참조값을 저장할 필드)
+		private Socket socket;
+		//클라이언트에게 문자열을 출력할 객체의 참조값을 저장할 필드
+		private BufferedWriter bw; 
+		
+		//생성자
+		public ServerThread(Socket socket) {
+			//생성자의 인자로 전달받은 Socket 객체의 참조값을 필드에 저장하기 
+			this.socket=socket;
+		}
+		//인자로 전달되는 문자열을 Socket 객체를 통해서 출력하는 메소드 
+		public void broadcast(String msg) throws IOException {
+			//인자로 전달된 문자열을 필드에 저장된 BufferedWriter 객체의 참조값을 이용해서
+			//클라이언트에게 출력하기 
+			bw.write(msg);
+			bw.newLine();//개행기호
+			bw.flush();//방출 
+		}
+		
+		@Override
+		public void run() {
+			try {
+				//클라이언트가 전송하는 문자열을 읽어들일 객체 
+				BufferedReader br=
+					new BufferedReader(
+							new InputStreamReader(socket.getInputStream()));
+				//클라이언트에게 문자열을 출력할 객체
+				bw=new BufferedWriter(
+							new OutputStreamWriter(socket.getOutputStream()));
+				//반복문 돌면서 클라이언트가 전송하는 문자열이 있는지 읽어와 본다.
+				while(true) {
+					//문자열 한줄이 전송될때 까지 블록킹 되는 메소드 
+					String line=br.readLine(); //클라이언트가 전송한 문자열을 읽어와서
+					//반복문 돌면서 모든 클라이언트에게 전송하기 
+					for(ServerThread tmp:threadList) {
+						tmp.broadcast(line);
+					}
+				}
+			}catch(Exception e) {
+				e.printStackTrace();
+			}finally {
+				try {
+					socket.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			//현재 스레드를 목록에서 제거하기 
+			threadList.remove(this);
+		}
+	}
+}
+
+
+
+
+
